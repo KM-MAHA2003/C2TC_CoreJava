8f1660f775fea874ae1e04c485c904767caa99d2
ELCOT <ELCOT@mahalakshmi>
2024-09-05 09:28:17 +0530

    this is my second commit

--- c2tc_maha/src/com/TNS/day2/SecondProgram.java
+++ c2tc_maha/src/com/TNS/day2/SecondProgram.java
@@ -4,7 +4,20 @@ public class SecondProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        //simple if
+		int a=10,b=5,c=15;
+		if(a>b) {
+			System.out.println("a is greater than b");	
+			}
+		//if else
+		if(a>b) {
+			System.out.println("a is greater than b");
+		}
+		else {
+			System.out.println("b is greater than a");
+			
+		}
+		
 	}
 
 }
---
+++ c2tc_maha/src/com/TNS/day2/ThirdProgram.java
@@ -0,0 +1,16 @@
+package com.TNS.day2;
+
+public class ThirdProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //for loop
+	    
+	    for (int i=0;i<=10;i++) {
+	          System.out.println(i);
+	        }
+	     
+	 
+	}
+
+}
